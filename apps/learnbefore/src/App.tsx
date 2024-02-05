277d074a47ba75ef6ff8dd551ba308acc66fa075
evgenius1424 <evgenius1424@yandex.ru>
2024-02-06 00:16:06 +0100

    feat: Add initial project files and configurations
    This commit adds the initial project files and configurations for the `learnbefore` application. The following changes have been made:
    
    - Added `.eslintrc.cjs` file for ESLint configurations.
    - Added `.gitignore` file to ignore logs, node_modules, and editor directories and files.
    - Added `README.md` file with basic project information.
    - Added `index.html` file with the basic structure of a HTML page.
    - Added `package.json` file with project dependencies and scripts.
    - Added `public/vite.svg` file as an icon.
    - Added `src/App.css` file for basic styling.
    - Added `src/App.tsx` file with the initial React component.
    - Added `src/index.css` file for global styles.
    - Added `src/main.tsx` file to bootstrap the React application.
    - Added `src/vite-env.d.ts` file for Vite environment types.
    - Added `tsconfig.json` and `tsconfig.node.json` files for TypeScript configurations.
    - Added `vite.config.ts` file for Vite configurations.
    
    These changes lay the groundwork for the `learnbefore` application and set up the necessary environment, dependencies, and initial files.

---
+++ apps/learnbefore/src/App.tsx
@@ -0,0 +1,23 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>LearnBefore™</h1>
+      <p className="description">
+        LearnBefore is a platform designed to help you discover unfamiliar
+        words, allowing you to learn them in advance, before encountering them
+        in films, books, blogs, etc.
+      </p>
+      <a
+        href="https://github.com/evgenius1424/learnbefore"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        See more on Github
+      </a>
+    </>
+  );
+}
+
+export default App;
