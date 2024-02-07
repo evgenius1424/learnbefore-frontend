2042f388f2fe4ab945724e736fc3fc9282daca21
evgenius1424 <evgenius1424@yandex.ru>
2024-02-07 20:23:01 +0100

    install shadcn/ui

--- apps/learnbefore/src/App.tsx
+++ apps/learnbefore/src/App.tsx
@@ -1,21 +1,23 @@
 import "./App.css";
+import { Alert, AlertDescription, AlertTitle } from "./components/ui/alert";
+import { BookOpenCheck } from "lucide-react";
 
 function App() {
   return (
     <>
-      <h1>LearnBefore™</h1>
-      <p className="description">
-        LearnBefore is a platform designed to help you discover unfamiliar
-        words, allowing you to learn them in advance, before encountering them
-        in films, books, blogs, etc.
-      </p>
-      <a
-        href="https://github.com/evgenius1424/learnbefore"
-        target="_blank"
-        rel="noreferrer noopener"
-      >
-        See more on Github
-      </a>
+      <Alert>
+        <BookOpenCheck className="h-4 w-4" />
+        <AlertTitle>LearnBefore™</AlertTitle>
+        <AlertDescription>
+          LearnBefore is a platform designed to help you discover unfamiliar
+          words, allowing you to learn them in advance, before encountering them
+          in films, books, blogs, etc.          <a
+            href="https://github.com/evgenius1424/learnbefore"
+            target="_blank"
+            rel="noreferrer noopener"
+          ></a>
+        </AlertDescription>
+      </Alert>
     </>
   );
 }
