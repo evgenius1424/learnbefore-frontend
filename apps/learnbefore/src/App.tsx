7c2a79e0ddd81ac0b947adf87aea5a16ce3c6ea6
evgenius1424 <evgenius1424@yandex.ru>
2024-02-11 00:31:50 +0100

    feat: Add Google OAuth support for testing

--- apps/learnbefore/src/App.tsx
+++ apps/learnbefore/src/App.tsx
@@ -1,3 +1,4 @@
+import { GoogleLogin } from "@react-oauth/google";
 import "./App.css";
 import { Alert, AlertDescription, AlertTitle } from "./components/ui/alert";
 import { BookOpenCheck } from "lucide-react";
@@ -11,12 +12,22 @@ function App() {
         <AlertDescription>
           LearnBefore is a platform designed to help you discover unfamiliar
           words, allowing you to learn them in advance, before encountering them
-          in films, books, blogs, etc.          <a
+          in films, books, blogs, etc.{" "}
+          <a
             href="https://github.com/evgenius1424/learnbefore"
             target="_blank"
             rel="noreferrer noopener"
           ></a>
         </AlertDescription>
+        <GoogleLogin
+          auto_select
+          onSuccess={(credentialResponse) => {
+            console.log(credentialResponse);
+          }}
+          onError={() => {
+            console.log("Login Failed");
+          }}
+        />
       </Alert>
     </>
   );
